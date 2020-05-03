76a95a0fe00f888d6160c04237086522b39edf25
Andreis3 <andrei.as3@hotmail.com>
2020-05-02 23:18:43 -0300

    add cors

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 
 import routes from './routes/index';
@@ -10,6 +11,7 @@ import AppError from './error/AppError';
 import './database';
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
@@ -32,7 +34,7 @@ app.use(
   },
 );
 
-const port = 3000;
+const port = 3333;
 
 app.listen(port, () => {
   console.log(`🚀 Runing service in port: ${port}`);
