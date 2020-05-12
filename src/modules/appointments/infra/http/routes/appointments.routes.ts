2519dcf68ca3f2fe341615f06da7346a254f5c1e
Andreis3 <andrei.as3@hotmail.com>
2020-05-11 23:31:53 -0300

    refactor domain appointment

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,30 +1,31 @@
 import { Router } from 'express';
 import { parseISO } from 'date-fns';
-import { getCustomRepository } from 'typeorm';
 
-import AppointmentsRepository from '@modules/appointments/repositories/AppointmentsRepository';
+import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
 import CreateServiceAppointments from '@modules/appointments/services/CreateAppointmentService';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
 const appointmentsRoutes = Router();
 
+const appointmentsRepository = new AppointmentsRepository();
+
 appointmentsRoutes.use(ensureAuthenticated);
 
-appointmentsRoutes.get('/', async (request, response) => {
-  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+// appointmentsRoutes.get('/', async (request, response) => {
+//   const appointments = await appointmentsRepository.find();
 
-  const appointments = await appointmentsRepository.find();
-
-  return response.status(200).json({ appointments });
-});
+//   return response.status(200).json({ appointments });
+// });
 
 appointmentsRoutes.post('/', async (request, response) => {
   const { provider_id, date } = request.body;
 
   const parseDate = parseISO(date);
 
-  const createdAppointment = new CreateServiceAppointments();
+  const createdAppointment = new CreateServiceAppointments(
+    appointmentsRepository,
+  );
 
   const appointment = await createdAppointment.execute({
     provider_id,
