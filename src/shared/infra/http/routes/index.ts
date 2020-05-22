d553fcafd8a3eb5967b0e27c7f8b0f6f9931c165
Andreis3 <andrei.as3@hotmail.com>
2020-05-21 22:19:02 -0300

    created reset password and token of expired reset password

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,14 +1,16 @@
 import { Router } from 'express';
 
-import appointmentsRoutes from '@modules/appointments/infra/http/routes/appointments.routes';
-import usersRoutes from '@modules/users/infra/http/routes/user.routes';
-import sessionsRoutes from '@modules/users/infra/http/routes/sessions.routes';
+import appointmentsRouter from '@modules/appointments/infra/http/routes/appointments.routes';
+import usersRouter from '@modules/users/infra/http/routes/user.routes';
+import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
+import passwordRouter from '@modules/users/infra/http/routes/password.routes';
 
 const routes = Router();
 
-routes.use('/users', usersRoutes);
+routes.use('/users', usersRouter);
 
-routes.use('/appointments', appointmentsRoutes);
-routes.use('/sessions', sessionsRoutes);
+routes.use('/appointments', appointmentsRouter);
+routes.use('/sessions', sessionsRouter);
+routes.use('/password', passwordRouter);
 
 export default routes;
